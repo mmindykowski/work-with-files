9bb0433da1dd64d3b2be9a156666d404593b313d
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-24 22:37:17 +0200

    obliczenie średniej

--- index.js
+++ index.js
@@ -28,11 +28,24 @@ fs.readdir(path.join(__dirname, "data"), function (err, files) {
         path.join(__dirname, "data", file),
         "utf-8",
         function (err, data) {
+          let sum = 0;
+          let studentsCount = 0;
+
           if (err) {
             console.log(err);
           } else {
             console.log(JSON.parse(data));
+
+            let studentsData = JSON.parse(data);
+
+            for (const studentData of studentsData) {
+              sum += studentData.mark;
+              studentsCount++;
+            }
           }
+
+          console.log(sum);
+
           fs.writeFile(
             path.join(__dirname, "average", "result.txt"),
             "test",
