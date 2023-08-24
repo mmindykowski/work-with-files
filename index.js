e463197369b316255cb715bc53c6074cd8da4cf6
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-24 22:27:50 +0200

    Stworzenie folderu i pliku txt z tekstem test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ fs.readdir(path.join(__dirname, "data"), function (err, files) {
   } else {
     console.log(files);
 
-    // tworzymy plik average
+    // tworzymy folderu average
 
     fs.mkdir(path.join(__dirname, "average"), function (err) {
       if (err) {
@@ -16,6 +16,19 @@ fs.readdir(path.join(__dirname, "data"), function (err, files) {
         console.log("Stworzono folder!");
       }
     });
+
+    // tworzenie pliku result.txt
+    fs.writeFile(
+      path.join(__dirname, "average", "result.txt"),
+      "test",
+      function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Stworzono plik!");
+        }
+      }
+    );
 
     // files.forEach(function (file) {
     //   fs.readFile(
