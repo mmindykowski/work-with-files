125a23957bd0e303f9904087e2a99c0cc143b158
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-24 22:20:24 +0200

    stworzenie folderu average metoda mkdir

--- index.js
+++ index.js
@@ -7,18 +7,28 @@ fs.readdir(path.join(__dirname, "data"), function (err, files) {
   } else {
     console.log(files);
 
-    files.forEach(function (file) {
-      fs.readFile(
-        path.join(__dirname, "data", file),
-        "utf-8",
-        function (err, data) {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log(JSON.parse(data));
-          }
-        }
-      );
+    // tworzymy plik average
+
+    fs.mkdir(path.join(__dirname, "average"), function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Stworzono folder!");
+      }
     });
+
+    // files.forEach(function (file) {
+    //   fs.readFile(
+    //     path.join(__dirname, "data", file),
+    //     "utf-8",
+    //     function (err, data) {
+    //       if (err) {
+    //         console.log(err);
+    //       } else {
+    //         console.log(JSON.parse(data));
+    //       }
+    //     }
+    //   );
+    // });
   }
 });
