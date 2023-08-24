840c7b3fc33dd8938d0fda480af3931d1421f722
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-24 21:57:32 +0200

    sprawdzenie plikowm czy istnieja

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const fs = require("fs");
 const path = require("path");
+
+fs.readdir(path.join(__dirname, "data"), function (err, files) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(files);
+  }
+});
