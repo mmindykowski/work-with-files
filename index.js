798002d348c1c21fc4f598085c2c4ced8da59d67
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-24 22:07:47 +0200

    3 parametr przed callback - typ kodowania

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ fs.readdir(path.join(__dirname, "data"), function (err, files) {
     console.log(files);
 
     files.forEach(function (file) {
-      fs.readFile(path.join(__dirname, "data", file), function (err, data) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(data.toString());
+      fs.readFile(
+        path.join(__dirname, "data", file),
+        "utf-8",
+        function (err, data) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(data);
+          }
         }
-      });
+      );
     });
   }
 });
